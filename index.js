92735c5780f1826ac4418d17c759ae8765a1c9bb
MereRo02 <roudeenr@gmail.com>
2025-08-13 18:18:46 +0200

    updated time &cities

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ let londonTimeElement = londonElement.querySelector(".time");
 
 	let newyorkElement = document.querySelector("#newyork");
 	let newyorkDateElement = newyorkElement.querySelector(".date");
-let newyorkTimeElement = newyorkElement.querySelector(".time");
+  let newyorkTimeElement = newyorkElement.querySelector(".time");
 	let newyorkTime = moment().tz("New York");
   
 	newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
   newyorkTimeElement.innerHTML = newyorkTime.format("h:mm:ss [<small>]A[</small>]");
+
+	let joburgElement = document.querySelector("#joburg");
+	let joburgDateElement = joburgElement.querySelector(".date");
+  let joburgTimeElement = joburgElement.querySelector(".time");
+	let joburgTime = moment().tz("Johannesburg");
+	
+	joburgDateElement.innerHTML = joburgTime.format("MMMM Do YYYY");
+	joburgTimeElement.innerHTML = joburgTime.format("h:mm:ss [<small>]A[</small>]");
 }
 updateTime();
 setInterval(updateTime, 1000);
