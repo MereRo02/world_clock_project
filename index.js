6aef8df02404f6a76cad4ab3441d072c57e759cc
MereRo02 <roudeenr@gmail.com>
2025-08-13 18:10:07 +0200

    updated javascript

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert("test");
+function updateTime() {
+	let londonElement = document.querySelector("#london");
+	let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+	let londonTime = moment().tz("London");
+  
+	londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+
+	let newyorkElement = document.querySelector("#newyork");
+	let newyorkDateElement = newyorkElement.querySelector(".date");
+let newyorkTimeElement = newyorkElement.querySelector(".time");
+	let newyorkTime = moment().tz("New York");
+  
+	newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
+  newyorkTimeElement.innerHTML = newyorkTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
