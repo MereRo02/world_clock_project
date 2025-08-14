f09b3f1b418018a064c67141902a5bcc57aa0941
MereRo02 <roudeenr@gmail.com>
2025-08-14 12:45:53 +0200

    updated select functions

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-function updateCities(event) {
+function updateCity(event) {
   let cityTimeZone = event.target.value;
 
   if (cityTimeZone.length > 0) {
     let cityNameParts = cityTimeZone.replace("_", " ").split("/");
-    let cityName = cityNameParts[1]; 
+    let cityName = cityNameParts[1];
 
     let cityTime = moment().tz(cityTimeZone);
+    let timezonesElement = document.querySelector("#timezones");
 
-    let citiesElement = document.querySelector("#timezones");
-    citiesElement.innerHTML = `
+    timezonesElement.innerHTML = `
       <div class="city">
         <div>
           <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+        <div class="time">
+          ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
+        </div>
       </div>
     `;
   }
 }
 
-setInterval(function () {
-  let selectElement = document.querySelector("#cities");
-  let selectedCity = selectElement.value;
+setInterval(() => {
+  let select = document.querySelector("#cities");
+  let selectedZone = select.value;
 
-  if (selectedCity) {
+  if (selectedZone) {
     
-    updateCities({ target: { value: selectedCity } });
+    updateCity({ target: { value: selectedZone } });
   }
 }, 1000);
 
-
-let citiesSelectElement = document.querySelector("#cities");
-citiesSelectElement.addEventListener("change", updateCities);
+let selectElement = document.querySelector("#cities");
+selectElement.addEventListener("change", updateCity);
